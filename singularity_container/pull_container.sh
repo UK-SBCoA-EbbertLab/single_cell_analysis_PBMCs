a910d256df684e05dabc5991210c21a7e726fe1a
Patricia Doyle <phdo222@mcc-login001.localdomain>
2025-02-28 18:06:57 -0500

    Updated pull_container.sh to pull the latest container from Singularity Library

--- singularity_container/pull_container.sh
+++ singularity_container/pull_container.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # analysis container
-singularity pull --arch amd64 
+#singularity pull --arch amd64 
+
+# Pull the latest version of the single-cell analysis container
+singularity pull library://phdo222/single_cell_data/single_cell_analysis:2025-02-24
 
 #TODO: fill in the line above
